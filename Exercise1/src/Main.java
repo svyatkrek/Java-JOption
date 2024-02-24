7361a735cd0ed8df1dd6703a7c434d92f7712de2
Mark <sviatik.krek@gmail.com>
2024-02-24 11:21:58 +0300

    first commit

---
+++ Exercise1/src/Main.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class Main{
+    public static class WindowApp extends JFrame { //Наследуя от JFrame мы получаем всю функциональность окна
+        private JButton displayMessage;
+        private JFrame layout;
+        private JPanel buttonsPanel;
+
+        public WindowApp() {
+            super("My First Window");
+            //
+            //
+            layout = new JFrame();
+            layout.setTitle("Task Synchronization ");
+            layout.setBounds(100, 100, 800, 600);
+            layout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            layout.getContentPane().setLayout(null);
+            //
+            //
+            displayMessage = new JButton("Открыть диалоговое окно");
+            layout.getContentPane().add(displayMessage);
+            //
+            //
+            buttonsPanel = new JPanel(new FlowLayout());
+            buttonsPanel.setBounds(200, 200, 300, 100);
+            layout.getContentPane().add(buttonsPanel);
+            buttonsPanel.add(displayMessage);
+
+            displayMessage.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    JOptionPane.showMessageDialog(layout, "Марк");
+                }
+            });
+        }
+    }
+
+    public static void main(String[] args){
+        WindowApp app = new WindowApp();
+        app.layout.setVisible(true);
+    }
+}
